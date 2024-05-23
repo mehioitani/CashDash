634b47011e2516cfd45940de56f7b35f44190e8a
mehioitani <muhieddineitani04@gmail.com>
2024-05-24 02:35:02 +0300

    Implement Transactions table

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -8,7 +8,7 @@ import { getLoggedInUser } from "@/lib/actions/user.actions";
 const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
   const currentPage = Number(page as string) || 1
   const loggedIn = await getLoggedInUser();
-  const accounts = await getAccounts({ userId: loggedIn.$id });
+  const accounts = await getAccounts({ userId: loggedIn?.$id });
   if (!accounts) return;
 
   const accountsData = accounts?.data;
--- components/RecentTransactions.tsx
+++ components/RecentTransactions.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { BankTabItem } from "./BankTabItem";
+import BankInfo from "./BankInfo";
+import TransactionsTable from "./TransactionsTable";
 
 const RecentTransactions = ({
   accounts,
@@ -23,14 +25,32 @@ const RecentTransactions = ({
         <TabsList className="recent-transactions-tablist">
           {accounts.map((account: Account) => (
             <TabsTrigger key={account.id} value={account.appwriteItemId}>
-              <BankTabItem 
-              key={account.id}
-              account={account}
-              appwriteItemId={appwriteItemId}
+              <BankTabItem
+                key={account.id}
+                account={account}
+                appwriteItemId={appwriteItemId}
               />
             </TabsTrigger>
           ))}
         </TabsList>
+        {accounts.map((account: Account) => (
+          <TabsContent
+            value={account.appwriteItemId}
+            key={account.id}
+            className="space-y-4"
+          >
+            <BankInfo
+              account={account}
+              appwriteItemId={appwriteItemId}
+              type="full"
+            />
+
+            <TransactionsTable
+            transactions={transactions}
+            />
+
+          </TabsContent>
+        ))}
       </Tabs>
     </section>
   );
